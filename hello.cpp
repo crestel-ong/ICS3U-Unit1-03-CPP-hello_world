194e770dc4ce1646d336bed32dae0d0c7b6506de
crestel-ong <crestel.ong@mths.ca>
2021-09-12 22:28:58 +0000

    properly styled code fourthtry

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,9 @@
 // Created by: Crestel Ong
 // Created on: September 2021
 // This is the "Hello, World!" program, with proper style
+
 #include <iostream>
+
 int main() {
-std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 }
