5414c45291b3f1ad6353783464de7419b4abcb96
crestel-ong <crestel.ong@mths.ca>
2021-09-13 22:15:01 +0000

    proper linked repo

---
+++ helloWorld2.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Crestel Ong All rights reserved
+// Created by: Crestel Ong
+// Created on: September 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
